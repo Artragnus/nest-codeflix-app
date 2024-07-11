24cbd5dd749f53afaf62d3d966d7a492302163a3
João Paulo <joaopptt@hotmail.com>
2024-07-10 21:40:40 -0300

    feat: :sparkles: create fake builder to category to improve tests

--- src/category/domain/__tests__/category.entity.spec.ts
+++ src/category/domain/__tests__/category.entity.spec.ts
@@ -1,6 +1,5 @@
-import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
-import { Category } from "../domain/category.entity";
-
+import { Uuid } from "../../../shared/domain/value-objects/uuid.vo";
+import { Category } from "../category.entity";
 describe("Category Unit Tests", () => {
   let validateSpy: any;
   beforeEach(() => {

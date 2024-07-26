b4c693297d6e5e421b359fddf7a590b4357b293b
João Paulo <joaopptt@hotmail.com>
2024-07-26 10:36:28 -0300

    refactor: change entity interface to have a notification

--- jest.d.ts
+++ jest.d.ts
@@ -3,7 +3,10 @@ import { FieldsErrors } from "./src/shared/domain/validators/validators-fields-i
 declare global {
   namespace jest {
     interface Matchers<R> {
-      containsErrorMessages(expected: FieldsErrors): R;
+      // containsErrorMessages(expected: FieldsErrors): => R;
+      notificationContainsErrorMessages: (
+        expected: Array<string | { [key: string]: string[] }>
+      ) => R;
     }
   }
 }
